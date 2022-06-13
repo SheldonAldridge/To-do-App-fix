fc341c2728bca66752fa677b6e8e55bc7dadc7b2
Sheldon <sheldonaldridge@yahoo.com>
2022-06-13 02:22:15 +0200

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//get variables
+let userInput = document.querySelector(".user-input");
+let addButton = document.querySelector(".add");
+let date = document.querySelector(".date");
+let time = document.querySelector(".time");
+
+//local storage key
+
+//variable of empty array that gets new task
+let taskList =[];
+
+// function that renders task list
+
+//function that creates new tasks with date and time
+function createTask(){
+
+}
+
+//event listner that listens for add button.
+//function that stores task list.
+
+//event listener that removes task from array.
+//function that removes stored task when deleted.
+
+//event listener that edits tasks with date.
+
+//event listener that completes task.
+
+
+

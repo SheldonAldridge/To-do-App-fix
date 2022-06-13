5caf6e336104afe298c73f3701d04b1cff8476b3
Sheldon <sheldonaldridge@yahoo.com>
2022-06-13 16:10:50 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-//get variables
-let userInput = document.querySelector(".user-input");
-let addButton = document.querySelector(".add");
-let date = document.querySelector(".date");
-let time = document.querySelector(".time");
+// variables object
 
+const el = {
+  form: document.querySelector(".form"),
+  input: document.querySelector(".user-input"),
+  date: document.querySelector(".date"),
+  time: document.querySelector(".time"),
+  
+};
 //local storage key
+
+const storage_key = "tasks-storage-key";
+
+//Create ID
+
+const createID = () => `${Math.floor(Math.random() * 10000)}-${new Date().getTime()}`
 
 //variable of empty array that gets new task
 let taskList =[];
@@ -12,11 +21,37 @@ let taskList =[];
 // function that renders task list
 
 //function that creates new tasks with date and time
-function createTask(){
+function addTask() {
 
-}
+  const id = createID();
+  const taskNew = el.input.value;
+  const taskDate = el.date.value;
+  const taskTime = el.time.value;
 
+  if (taskNew != null) {
+
+    const tasks = document.createElement("div");
+    tasks.innerHTML = `
+        <div class="task-content">
+        <div class="list-of-task">
+        <div class="id">${id}</div>
+        <input type="checkbox" class="tick">
+        <div class="new-task-created">${taskNew}</div>
+        <label class="due-date">${taskDate}</label>
+        <label class="due-time">${taskTime}</label>
+    </div>
+
+    <div class="atcion-buttons">
+        <button class="edit" data-id="">Edit</button>
+        <button class="delete" data-id="">Delete</button>
+    </div>
+</div>`
+
+  };
+  return taskNew
+};
 //event listner that listens for add button.
+
 //function that stores task list.
 
 //event listener that removes task from array.

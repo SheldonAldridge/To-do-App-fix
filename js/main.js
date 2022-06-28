0ba95096580a90290657587ed2c26de27bfb0ed6
Sheldon <sheldonaldridge@yahoo.com>
2022-06-29 01:42:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,9 +18,7 @@ const createId = () =>
 //variable of empty array that gets new task
 let taskList = JSON.parse(localStorage.getItem(STORAGE_KEY) ?? "[]");
 
-
 function makeNewTask() {
-
   const data = {
     id: createId(),
     taskNew: el.input.value,
@@ -28,9 +26,8 @@ function makeNewTask() {
     taskTime: el.time.value,
   };
 
-  return data
+  return data;
 }
-
 
 //function that creates new tasks with date and time
 function display() {
@@ -39,21 +36,19 @@ function display() {
   data = makeNewTask();
 
   tasks.innerHTML = `
-       <div class="task-content">
-         <div class="item-content">
-        <div class="task" data-id="${data.id}">
-        <div class="new-task-created">${data.taskNew}</div>
-        <label class="due-date">${data.taskDate}</label>
-        <label class="due-time">${data.taskTime}</label>
-        </div>
-    </div>
-
-    <div class="action-buttons">
-        <button onclick="editItem()" class="edit" data-id="${data.id}">Edit</button>
-        <button onclick="deleteItem()" class="delete" data-id="${data.id}">Delete</button>
-        <button onclick="completeItem()" class="complete" data-id="${data.id}">Complete</button>
-    </div>
-</div>`;
+         <div class="task-content">
+          <div class="task" data-id="${data.id}">
+          <div class="new-task-created">${data.taskNew}</div>
+          <label class="due-date">${data.taskDate}</label>
+          <label class="due-time">${data.taskTime}</label>
+      </div>
+  
+      <div class="action-buttons">
+          <button onclick="editItem(event)" class="edit" data-id="${data.id}">Edit</button>
+          <button onclick="deleteItem(event)" class="delete" data-id="${data.id}">Delete</button>
+          <button onclick="completeItem(event)" class="complete" data-id="${data.id}">Complete</button>
+      </div>
+  </div>`;
 
   taskList.push(tasks);
   el.list.appendChild(tasks);
@@ -71,8 +66,8 @@ function storeList() {
 
 //function that removes task from array with delete button.
 
-function deleteItem() {
-  let removeitem = document.querySelector(".task-content");
+function deleteItem(event) {
+  let removeitem = event.target.closest(".task-content");
   removeitem.parentNode.removeChild(removeitem);
   localStorage.removeItem(STORAGE_KEY);
 }
@@ -80,11 +75,33 @@ function deleteItem() {
 //function that removes stored task when deleted.
 
 //function that that edits tasks with date and time.
-function editItem(){
+function editItem() {
+  let edit = document.querySelector(".modal");
+  edit.style.display ="block";
+}
+
+//Function replace updated item
+function updateTask(event){
+
+let close = document.querySelector(".close");
+console.log(close);
+let edit = document.querySelector(".modal");
+
+if(close){
+   edit.style.display = "none";
+}
+
 
 }
 
 //function that that completes task.
-function completeItem() {
-taskList.splice
+function completeItem(event) {
+  const element = event.target.closest(".task-content");
+  let taskItem = element.querySelector(".new-task-created");
+  let dateItem = element.querySelector(".due-date");
+  let timeItem = element.querySelector(".due-time");
+  // style..
+  taskItem.style.textDecoration = "line-through";
+  dateItem.style.textDecoration = "line-through";
+  timeItem.style.textDecoration = "line-through";
 }
